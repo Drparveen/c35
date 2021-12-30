63bd43670f94bb3d9960e309697353ea2fd5d533
Drparveen <84265153+Drparveen@users.noreply.github.com>
2021-12-29 23:51:57 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,15 +73,10 @@ function draw() {
 //   balloon.y = height.y;
 // }
 
- function readHeight(data){
-   height = data.val();
-   balloon.x = height.x;
-   balloon.y = height.y;
- }
 
-// function readHeight(data){
-//   height = data.val();
-// }
+ function readHeight(data){
+  height = data.val();
+}
 
 // function readHeight(){
 //   height = val();
